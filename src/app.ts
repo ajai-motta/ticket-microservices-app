3de52df7a1c04a5e8af0d1a3f3f1769ea232c871
Ajai Joseph <ajayjoseo8@gmail.com>
2025-09-10 10:21:46 +0530

    start testing(app.ts) added

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express";
+import cookieSession from "cookie-session";
+import { currentUserRouter } from "./routes/current-user";
+import { signinUserRouter } from "./routes/signin";
+import { signoutUserRouter } from "./routes/signout";
+import { signupUserRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+const app = express();
+
+
+// trust traffic even though its comming from proxy  
+app.set('trust proxy',true)//trust ingress-nginx
+//parse content
+app.use(express.json());
+//cookie, remember content is a jwt so no encryption on cookie
+app.use(cookieSession({
+  signed: false,
+  secure: true,// From https
+}))
+
+app.use(currentUserRouter)
+app.use(signinUserRouter)
+app.use(signoutUserRouter)
+app.use(signupUserRouter)
+app.all('/{*splat}', async() => {
+  throw new NotFoundError()
+});
+app.use(errorHandler)
+
+export {app}
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,5 @@
-import express, { Request, Response } from "express";
-import cookieSession from "cookie-session";
-import { currentUserRouter } from "./routes/current-user";
-import { signinUserRouter } from "./routes/signin";
-import { signoutUserRouter } from "./routes/signout";
-import { signupUserRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
 import mongoose from 'mongoose'
-const app = express();
-const PORT = 3000;
-
-// trust traffic even though its comming from proxy  
-app.set('trust proxy',true)//trust ingress-nginx
-//parse content
-app.use(express.json());
-//cookie, remember content is a jwt so no encryption on cookie
-app.use(cookieSession({
-  signed: false,
-  secure: true,// From https
-}))
-
-app.use(currentUserRouter)
-app.use(signinUserRouter)
-app.use(signoutUserRouter)
-app.use(signupUserRouter)
-app.all('/{*splat}', async() => {
-  throw new NotFoundError()
-});
-app.use(errorHandler)
+import { app } from './app'
 
 const startUp=async()=>{
   if(!process.env.JWT_KEY){
@@ -41,7 +13,7 @@ const startUp=async()=>{
     console.log(err)
   }
 
-  app.listen(PORT, () => {
+  app.listen(3000, () => {
     console.log(`🚀 Server is running `);
   });
 }
